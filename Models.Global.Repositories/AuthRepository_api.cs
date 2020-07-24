8d9f8a17af9e5d2996381c306e8b3dcfe3dfa060
mikatournay@gmail.com <mikatournay@gmail.com>
2020-07-24 11:16:18 +0200

    Mise en place du controller API

---
+++ Models.Global.Repositories/AuthRepository_api.cs
@@ -0,0 +1,21 @@
+﻿using Model.Interfaces;
+using Models.Global.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models.Global.Repositories
+{
+    class AuthRepository_api : IAuthRepository<UserGlobal>
+    {
+        public UserGlobal Login(UserGlobal entity)
+        {
+
+        }
+
+        public void Register(UserGlobal entity)
+        {
+
+        }
+    }
+}
---
+++ webAPI/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Model.Interfaces;
+using Models.Global.Entities;
+using Models.Global.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace webAPI.Controllers
+{
+    
+
+    public class AuthController : ApiController
+    {
+        public IAuthRepository<UserGlobal> _repo;
+
+        public AuthController()
+        {
+            _repo = new AuthRepository();
+        }
+
+        [HttpPost]
+        [Route("api/Auth/Login")]
+        public UserGlobal Login([FromBody] UserGlobal entity)
+        {
+            return _repo.Login(entity);
+        }
+
+
+        [HttpPost]
+        [Route("api/Auth/Register")]
+        public void Register([FromBody] UserGlobal entity)
+        {
+            _repo.Register(entity);
+        }
+    }
+}
